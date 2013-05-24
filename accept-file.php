c3b29224768b423058171801460df7c948589760
bernte <bernte@gmail.com>
2013-05-24 14:28:39 +0300

    Update accept-file.php
    made it shorter

--- accept-file.php
+++ accept-file.php
@@ -6,9 +6,9 @@ if ($_FILES['fileToUpload']['error'] > 0) {
     echo "Error: " . $_FILES['fileToUpload']['error'] . "<br />";
 } else {
     // array of valid extensions
-    $validExtensions = array('.jpg', '.jpeg', '.JPG', '.JPEG', '.GIF', '.PNG', '.gif', '.png');
+    $validExtensions = array('.jpg', '.jpeg', '.gif', '.png');
     // get extension of the uploaded file
-    $fileExtension = strrchr($_FILES['fileToUpload']['name'], ".");
+    $fileExtension = strtolower(strrchr($_FILES['fileToUpload']['name'], "."));
     // check if file Extension is on the list of allowed ones
     if (in_array($fileExtension, $validExtensions)) {
         $newNamePrefix = time() . '_';
